237531d1457bcb94238f656436cc9e6e53ce8bdd
Javier Augusto Raffo Huamán <jraffoh@kapcomperu.com>
2020-08-09 21:49:32 -0500

    fix

--- criterias/base/base.go
+++ criterias/base/base.go
@@ -1,12 +1,8 @@
 package base
 
 type AbstractCriteria struct {
-	Page    int64      `schema:"page"`
-	Limit   int64      `schema:"limit"`
-	Sorting []SortType `schema:"sorting"`
-}
-
-type SortType struct {
+	Page          int64  `schema:"page"`
+	Limit         int64  `schema:"limit"`
 	SortBy        string `schema:"sortBy"`
-	Direction string `schema:"direction"`
+	SortDirection string `schema:"sortDirection"`
 }
